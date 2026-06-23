893793f689ffc440655aef18c66cf05d5e48373a
banexxl <damjanovic.branislav@gmail.com>
2026-06-23 19:07:07 +0200

    local products

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,21 +1,25 @@
 import { type NextRequest, NextResponse } from "next/server"
+import products from "./products.json"
 
 export async function GET(request: NextRequest) {
   try {
     const { searchParams } = new URL(request.url)
     const category = searchParams.get("category")
 
-    let url = "https://fakestoreapi.com/products"
-    if (category) {
-      url += `/category/${category}`
-    }
+    const filteredProducts = category
+      ? products.filter(
+        (product) =>
+          product.category.toLowerCase() === category.toLowerCase()
+      )
+      : products
 
-    const response = await fetch(url)
-    console.log("Fetched products from API:", response)
-    const products = await response.json()
-
-    return NextResponse.json(products)
+    return NextResponse.json(filteredProducts)
   } catch (error) {
-    return NextResponse.json({ error: "Failed to fetch products" }, { status: 500 })
+    console.error(error)
+
+    return NextResponse.json(
+      { error: "Failed to fetch products" },
+      { status: 500 }
+    )
   }
 }
